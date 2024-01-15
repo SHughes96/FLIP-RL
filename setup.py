ca0790e9559877c4e2d1562f498a95f548cfb35b
Sarah Hughes <sarah.hughes@ast.cam.ac.uk>
2024-01-15 15:14:22 +0000

    Made a field setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
+import numpy as np
 
 setup(name='Delta_RL',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
+      install_requires=['gym',
+                        'numpy'
+                        ]  # And any other dependencies foo needs
 )
+
