c8435e4f888c7aa335b9a7eb761d981ba8b530d6
Sarah Hughes <sarah.hughes@ast.cam.ac.uk>
2024-03-15 16:57:13 +0000

    Adding in function to calculate the time taken for a move

--- create_env.py
+++ create_env.py
@@ -16,6 +16,32 @@ class Delta(gym.Env):
         super(Delta, self).__init__()
         
         
+    def calculate_time(self, robotVect, fibre_start, fibre_end):
+        """ 
+        A function to calculate the time taken for a move
+        inputs
+        ---
+        robotVect: the current position of the robot coordintes
+        fibre_start: The starting position of the fibre
+        fibre_end: The end position of the fibre
+        
+        Output
+        ---
+        the time taken in seconds
+        """        
+        for arg in [robotVect, fibre_start, fibre_end]:
+            if not isinstance(arg, (list, tuple, np.ndarray)):
+                raise TypeError("All input variables must be arrays.")
+
+        
+        speed = 1.0 #placeholder value
+        distance_vect = np.abs(fibre_start - robotVect) - np.abs(fibre_end-fibre_start)
+        distance = np.sqrt(distance_vect[0]**2 + distance_vect[1]**2)
+        T = distance/speed
+        
+        return T
+        
+        
         
         
 if __name__ == "__main__":
