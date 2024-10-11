FIxing issues with the fibre distance calculation

Delta_RL/field_start.py
@@ -23,7 +23,24 @@ def calc_fib_coords(N, R=0.2):
         coordinates_radial.append((ID, R, theta))  
       
     return np.asarray(coordinates_cartesian), np.asarray(coordinates_radial)
-  
+
+def calc_fibre_dist(fibre_start, fibre_end):
+    '''A function to calculate the distance for a single fibre'''
+    try:
+        distances = []
+        for start, end in zip(fibre_start, fibre_end):
+            dist = np.linalg.norm(end - start)
+            #dist = np.sqrt(distance_vect[0]**2 + distance_vect[1]**2)
+
+            distances.append(dist)
+        return np.sum(distances)
+        
+    except:
+        ValueError('Could not evaluate minimum distance')
+    
+     
+
+ 
 
 def generate_points(coords, angle_range_degrees):
     """Generate random points within cones around each coordinate point.
@@ -45,26 +62,28 @@ def generate_points(coords, angle_range_degrees):
 
         # Generate random angle within cone range
         rand_ang = np.radians(np.random.uniform(-angle_range_degrees, angle_range_degrees))
-        # if rand_ang < 0:
-        #     rand_ang += 2*np.pi
-        
-        # Calculate radius to center of circle
-        #radius = np.sqrt(x**2 + y**2)
-        if theta < 0:
-            theta += 2*np.pi
 
         # Generate random radius within cone radius
-        rand_radius = (np.random.uniform(0.2, 1.2))
+        rand_radius = 1.2-np.sqrt(np.random.uniform(0., 1.))
+        
+        x_shift = rand_radius * np.cos(rand_ang)
+        y_shift = rand_radius * np.sin(rand_ang)
+        
+        rotate = np.array([[np.cos(-theta), -np.sin(-theta)],
+                       [np.sin(-theta), np.cos(-theta)]])
+        
+        x_shift,y_shift = np.dot(rotate,np.array([x_shift,y_shift]))
 
         # Calculate coordinates of new point
-        new_x = x  - rand_radius * np.cos(rand_ang + theta)
-        new_y = y - rand_radius * np.sin(rand_ang + theta)
+        new_x  = x - x_shift
+        new_y = y + y_shift
         
-        new_R = Rin - rand_radius
-        new_theta = rand_ang + theta
+        new_R = np.sqrt(new_x**2. + new_y**2.)
+        new_theta = np.arctan(y/x)
         
-        if new_theta < 0:
-            new_theta += 2*np.pi
+        if new_x < 0:
+            new_theta = np.pi - new_theta
+        
 
         targets_cartesian.append((new_x, new_y))
         targets_radial.append((new_R, new_theta))
@@ -108,6 +127,7 @@ def plot_field(start_coords, end_coords):
     ax.set_title('Unit Circle')
     
     plt.show()
+    
 
 class field_gen():
     def __init__(self, total_fibres):
@@ -116,20 +136,14 @@ class field_gen():
         
         self.fibre_positions_origin = start_coords[:, 1:]
         self.fibre_positions_origin_radial = radial_start[:, 1:]
-        #print('FIELD GEN COORDS: ', self.fibre_positions_origin_radial)
         
         self.ids = self.fibre_positions_origin[:, 0].astype(int)
         self.fibre_end_positions, self.fibre_ends_radial = generate_points(self.fibre_positions_origin_radial, 14)
         
-        
-        # print('start_positions', self.fibre_positions_origin[0:2])
-        # print('end_positions', self.fibre_end_positions[0:2])
-        #self.full_coords_list = np.hstack([start_coords, self.fibre_end_positions])
-         
         self.flattened_coords_cartesian = alternate_vectors(self.fibre_positions_origin, self.fibre_end_positions)
         self.flattened_coords_radial = alternate_vectors(self.fibre_positions_origin_radial, self.fibre_ends_radial)
-        #plot_field(np.array([[0,0], [1,0]]), np.array([[1, 1], [1,2]]))
-            
+        
+        self.minimum_distance = calc_fibre_dist(self.fibre_positions_origin, self.fibre_end_positions)
             
         return
     
@@ -138,11 +152,45 @@ class field_gen():
         return
         
 if __name__ == '__main__':
-    fg = field_gen(30)
-    #print(fg.flattened_coords_radial)
-    fg.field_img()
-    #print(fg.flattened_coords_radial)
+    fg = field_gen(100)
     
+    #fg.field_img()
+    test_pos = np.empty((1000,2))
+    print(test_pos[0])
+    for i in range(1000):
+        cart, rad = generate_points(np.array([[1.2,0. ]]), 14)
+        test_pos[i] = cart
+    
+    #fibs_cart, fibs_rad = calc_fib_coords(30, R=0.2)
+    #print(fibs_rad)
+    #fig, ax = plt.subplots()
+
+    # # Create theta values from 0 to 2*pi
+    # theta = np.linspace(0, 2*np.pi, 100)
+
+    # # Define x and y coordinates of the unit circle
+    # x = 1.2*np.cos(theta)
+    # y = 1.2*np.sin(theta)
+
+    # # Plot the unit circle
+    # ax.plot(x, y)
+    
+    # plt.scatter(test_pos[:, 0], test_pos[:, 1])
+    # plt.show()
+    # ends = fg.fibre_ends_radial
+    
+    
+    
+    
+    
+    #print(ends)
+    #fig = plt.figure()
+    #ax = fig.add_subplot(projection='polar')
+    #c = ax.scatter(fibs_rad[:, 2], fibs_rad[:, 1])
+    #plt.show()
+    #
+    #
+    #plt.show()
 
 
     